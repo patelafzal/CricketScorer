556f69a16abd58a5ac4ef30754b06656310cf394
Afzal Patel <afzal.patel09@outlook.com>
2025-03-22 11:00:57 +0530

    Intial code commit

---
+++ CrickerScorer/CrickerScorer/Enum/BallType.cs
@@ -0,0 +1,22 @@
+﻿namespace CrickerScorer.Enum
+{
+    public enum BallType
+    {
+        Legal,
+        NoBall,
+        WideBall,
+        DeadBall,
+        Wicket
+    }
+    public enum WicketType
+    {
+        None,
+        Bowled,
+        Caught,
+        RunOut,
+        Stumped,
+        LBW,
+        HitWicket
+    }
+
+}
