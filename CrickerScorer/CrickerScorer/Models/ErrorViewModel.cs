03c78b4acd4ea2719edab2d12c2b4c803a4beb97
Afzal Patel <afzal.patel09@outlook.com>
2025-03-22 10:54:28 +0530

    Initial commit

--- CrickerScorer/CrickerScorer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CrickerScorer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
